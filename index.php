e330c883438dfc0286feb27f88185a5a9866f3ff
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-08-17 11:07:41 +0200

    worked on PUT request. Not updating though

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 
 include 'DbConnect.php';
 
 $objDb = new DbConnect;
-$conn = $objDb->connect();
+$conn = $objDb->connect(); 
 
 //read data
 // POST does not read jason data
@@ -16,9 +18,23 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch($method){
     case "GET":
         $sql = "SELECT * FROM `react-crud`";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // get url and the split it at the /
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        // If statement for an individual user
+        if(isset($path[3]) && is_numeric($path[3])){
+            $sql .= "WHERE id = :id";
+            $stmt = $conn ->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            // if we are not working with an individual
+            //ut rather all the users
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
         echo json_encode($users);
         break;
 
@@ -42,5 +58,25 @@ switch($method){
         }
         echo json_encode($response);
         break;
+
+    case "PUT":
+        $user = json_decode(file_get_contents('php://input')); //so we can read the data in JSON
+        $sql = "UPDATE `react-crud` SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE `react-crud`.`id` =:id";
+        $stmt = $conn->prepare($sql);
+        $updated_at = date('Y-m-d');
+        
+        $stmt->bindParam(':id', $user->id);
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':updated_at', $updated_at);
+        
+        if($stmt->execute()){
+            $response = ['status' => 1, 'message' => 'User updated successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to update user.'];
+        }
+        echo json_encode($response);
+        break;    
 }
 
